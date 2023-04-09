cd74bd9d05cf56ced42afd97567aa39ac620d943
rayane <moryane900@gmail.com>
2023-04-09 23:42:59 +0100

    i can create checkboxes from the addtask class to the main one

--- app/src/main/java/com/example/medapp/MainActivity.kt
+++ app/src/main/java/com/example/medapp/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.CheckBox
 import android.widget.LinearLayout
+import android.widget.RelativeLayout
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import com.example.medapp.databinding.ActivityMainBinding
@@ -13,16 +14,20 @@ import com.example.medapp.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var checkboxContainer: LinearLayout
+    companion object {
+         var checkBox_id: Int = 1 // Define a static integer attribute
+    }
     private val addTaskLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
         if (result.resultCode == Activity.RESULT_OK) {
             val intent = result.data
             val bundle: Bundle? = intent?.extras
             val msg = bundle?.getString("AddedTask")
             Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
-            binding.tvTaskName.text = msg
 
             // Step 3: Dynamically create and add CheckBox elements
             val checkBox = CheckBox(this)
+            checkBox.id = checkBox_id // Set the ID for the CheckBox
+            checkBox_id += 1
             checkBox.text = msg
             checkBox.layoutParams = LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.WRAP_CONTENT,
@@ -34,13 +39,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
-
-        checkboxContainer = findViewById(R.id.checkboxContainer)
-
         setContentView(binding.root)
 
-
+        checkboxContainer = findViewById(R.id.checkboxContainer)
 
         binding.btnAddTask.setOnClickListener{
             Toast.makeText(this, "Task added !",Toast.LENGTH_SHORT).show()
