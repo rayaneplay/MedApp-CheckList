16dd5bc16acfa68f874cb075fa1f46bd60aa7239
rayane <moryane900@gmail.com>
2023-04-09 23:16:38 +0100

    addTask activity send string message to main activity which displays it

--- app/src/main/java/com/example/medapp/AddTaskActivity.kt
+++ app/src/main/java/com/example/medapp/AddTaskActivity.kt
@@ -1,13 +1,30 @@
 package com.example.medapp
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.medapp.databinding.ActivityAddtaskBinding
 
 class AddTaskActivity : AppCompatActivity(){
-
+    private lateinit var binding: ActivityAddtaskBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_addtask)
+
+        binding = ActivityAddtaskBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnConfirmAddTask.setOnClickListener{
+            val message: String = binding.etTaskName.text.toString()
+            Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("AddedTask",message)
+            setResult(Activity.RESULT_OK, intent)
+            finish()
+        }
+
     }
 }
--- app/src/main/java/com/example/medapp/MainActivity.kt
+++ app/src/main/java/com/example/medapp/MainActivity.kt
@@ -1,25 +1,55 @@
 package com.example.medapp
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.widget.CheckBox
+import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.medapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var checkboxContainer: LinearLayout
+    private val addTaskLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val intent = result.data
+            val bundle: Bundle? = intent?.extras
+            val msg = bundle?.getString("AddedTask")
+            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+            binding.tvTaskName.text = msg
+
+            // Step 3: Dynamically create and add CheckBox elements
+            val checkBox = CheckBox(this)
+            checkBox.text = msg
+            checkBox.layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            checkboxContainer.addView(checkBox)
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        checkboxContainer = findViewById(R.id.checkboxContainer)
+
         setContentView(binding.root)
+
+
+
         binding.btnAddTask.setOnClickListener{
             Toast.makeText(this, "Task added !",Toast.LENGTH_SHORT).show()
 
            val intent = Intent(this, AddTaskActivity ::class.java)
-            startActivity(intent)
+
+
+            addTaskLauncher.launch(intent)
+
         }
     }
-} 
+}
